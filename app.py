6624f4d1f23e0e9c383957ef3fca8af25c19b866
Shafiqur Rahman <52239911+shafiq-sanchy@users.noreply.github.com>
2025-11-02 21:20:07 +0600

    new filters

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide", page_title="Advanced Email Validator")
 
 # Exclusion lists
 EXCLUDED_KEYWORDS = [
-    "support@", "sales@", "team@", "hr@", "jobs@", "careers@", "press@", "media@", "partnerprogram@", "reservations@", "enquiries@",
+    "support@", "sales@", "team@", "hr@", "jobs@", "careers@", "press@", "media@", "partnerprogram@", "reservations@", "enquiries@", "copywrite@", "safety@",
     "privacy@", "security@", "abuse@", "noreply@", "no-reply@", "unsubscribe@", "your@", "yourfriend@", "yourfriends@", "name@",
     "newsletter@", "feedback@", "test@", "demo@", "example@", "dummy@", "yourname@", "youremail@", "abuse@", "dmca@", "security@", "stop", "noreply@", "no-reply@", "bot@", "john.doe@"
 ]
